d41b741615bee71922dd5574eca446493d6eb762
DavidTalamona <info@codeworx.ch>
2018-12-03 21:07:30 +0100

    Work around a limit of Stripe.js (Stripe Checkout)

--- src/android/ThemeableBrowser.java
+++ src/android/ThemeableBrowser.java
@@ -784,6 +784,7 @@ public class ThemeableBrowser extends CordovaPlugin {
                 settings.setBuiltInZoomControls(features.zoom);
                 settings.setDisplayZoomControls(false);
                 settings.setPluginState(android.webkit.WebSettings.PluginState.ON);
+                settings.setUserAgentString("Mozilla/5.0 (Linux; Android 4.4.4; One Build/KTU84L.H4) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/28.0.0.20.16;]");
 
                 //Toggle whether this is enabled or not!
                 Bundle appSettings = cordova.getActivity().getIntent().getExtras();
